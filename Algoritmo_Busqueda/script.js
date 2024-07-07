0b93b4eff95eaf5c1b3ba73952c866aca2bab35e
Raao-tech <rafaelaraujordo@gmail.com>
2024-07-07 04:25:03 -0400

    creacion de la funcion gridAtom() para poder organizar la curadricula en elementos de un array relacional

--- Algoritmo_Busqueda/script.js
+++ Algoritmo_Busqueda/script.js
@@ -1,6 +1,5 @@
-import { Player } from "./classes/Player.js";
 
-const player = new Player();
+
 
 const canvas = document.getElementById("canvas");
 const spaces = 10 | 0;
@@ -13,7 +12,10 @@ if (canvas.getContext) {
 
     let canvas_width = canvas.width | 0;
     let canvas_heigth = canvas.height | 0;
+    let Columns = {};
 
+
+    
 
 
 
@@ -21,16 +23,21 @@ if (canvas.getContext) {
     let unity_X = canvas_width/spaces | 0;
     let unity_Y = canvas_heigth/spaces | 0;
 
-
+   
+    //creación del array que encierra en elementos cada cuadricula del grid
+    gridAtom(canvas_width,canvas_heigth, spaces,Columns);
 
 
     //Trazado de la cuadricula/grid
     gridDraw( ctx, canvas_width,canvas_heigth, spaces);
 
-    ctx.fillRect(90, 0, spaces , spaces );
-    ctx.fillRect(120, 130, spaces , spaces );
+    let xs = Columns[0][3][0];// En la CoLumna 0, en la fila 3, en su coordenada X 
+    let ys =Columns[0][3][1];// En la Columna 0, en su fila 3, en su coordenada Y
 
-    
+    ctx.fillRect(Columns[0][0][0], Columns[0][0][1], spaces , spaces );
+    ctx.fillRect( xs,ys, spaces , spaces );
+
+    console.log(xs, ys);
 
     canvas.addEventListener("mousemove", function(event) {
         const rect = canvas.getBoundingClientRect(); // Get canvas position
@@ -47,6 +54,9 @@ if (canvas.getContext) {
     })
 
  
+
+   
+
 
 
 
@@ -105,4 +115,35 @@ function gridDraw(ctx, width,heigth, spaces) {
     
     
 }
+/*
+    @ width es el ancho del canvas
+    @ heigth es el alto del canvas
+    @ spaces dimensiones de los cuadrados de la cuadricula
+    @ Columns es el array que poseera todos lo elementos organizados de la cuadricula
+*/ 
+function gridAtom(width, heigth, spaces,Columns) {
+
+    let ColumnsFull = width/spaces + 1 | 0; //20 columnas
+    let RowsFull = heigth / spaces + 1| 0;//30 filas
+
     
+   
+
+    for (let C = 0; C <= ColumnsFull; C ++) {
+       
+        Columns[C] = [];
+        for (let R = 0; R <= RowsFull; R++) {
+           Columns[C].push([C * spaces,R * spaces]);
+            
+        }
+
+     
+        
+    }
+
+    
+    
+
+    return Columns;
+    
+}
